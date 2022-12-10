91d2e174f0fe32a0029bcd5b38b48b2262d08f3c
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-10 00:29:21 -0800

    removing the extra space on the put function

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,8 +12,8 @@ int main(void)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for(i = 0; i < 26; i++)
-		putchar( alphabet[i]);
+	for( i = 0; i < 26; i++)
+		putchar(alphabet[i]);
 	putchar('\n');
 
 	return (0);
