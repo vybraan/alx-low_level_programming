6d01270c1ffd405e879766095ed1a46ca082ec3d
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-10 13:12:22 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - takes the string of aphabet and prints
+ * all one by one in a loop except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (alphabet[i] != 'q' && alphabet[i] != 'e')
+			putchar(alphabet[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
