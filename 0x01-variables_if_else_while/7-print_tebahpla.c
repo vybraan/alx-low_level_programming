c4aeb50d5a16f233da90ece70bdcfcbe7d93fa15
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-10 13:36:30 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - takes the string of aphabet and prints
+ * all one by one in a loop
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i < 0; i--)
+		putchar(alphabet[i]);
+	putchar('\n');
+
+	return (0);
+}
