5568d20723e05a596800440c3f9dae90d4d63a11
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-11 16:14:49 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - takes the string of aphabet and prints
+ * all one by one in a loop
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char base16[] = "123456789abcdef";
+	int i;
+
+	for (i = 0; i < 16; i++)
+		putchar(base16[i]);
+	putchar('\n');
+
+	return (0);
+}
