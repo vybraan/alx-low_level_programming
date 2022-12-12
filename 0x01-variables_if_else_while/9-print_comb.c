90e28cddaf60dc97a0ee8473ea31e1f1b4d4609d
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-11 16:37:55 -0800

    betty coding style

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,19 +10,15 @@
 
 int main(void)
 {
-        int i;
-
-        for (i = 0; i < 10; i++)
-        {
-                putchar((char)i + '0');
-
-                if (i == 9)
-                        continue;
-                putchar(',');
-                putchar(' ');
-        }
-        putchar('\n');
-
-        return (0);
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar((char)i + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
-
