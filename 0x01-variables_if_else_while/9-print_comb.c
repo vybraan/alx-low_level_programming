0275bd6f098b9e03bdb92017728ef9223088c244
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-11 16:34:03 -0800

    print all from 0 to 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0, 1, - 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+        {
+                putchar((char)i + '0');
+
+                if (i == 9)
+                        continue;
+                putchar(',');
+                putchar(' ');
+        }
+        putchar('\n');
+
+        return (0);
+}
+
