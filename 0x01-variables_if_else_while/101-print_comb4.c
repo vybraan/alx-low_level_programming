df3bd505f2791c9b33157943fdbe39e66046b774
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-13 14:36:37 -0800

    adding a denial

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,7 @@ int main(void)
 				putchar((j % 10) + '0');
 				putchar((k % 10) + '0');
 
-				if (i == 7 && j == 8 && k == 9)
+				if (!(i == 7 && j == 8 && k == 9))
 				{
 					putchar(',');
 					putchar(' ');
