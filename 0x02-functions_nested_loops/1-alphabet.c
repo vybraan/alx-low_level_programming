a5d464173e98f3e0bbc83dcdb198fed5eb2a372a
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-13 15:27:20 -0800

    FIX: betty conding style and documentation

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,10 @@
 /**
  * main - Entry point.
  *
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
  * Return: 0 Success
  */
- 
 void print_alphabet(void)
 {
 	char c;
