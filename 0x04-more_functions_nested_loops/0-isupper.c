c50caddaebadaec2c641a0d5f37b013e4972dd23
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-23 11:01:19 -0800

    UPDATE: content

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,6 +6,17 @@
  *
  * Return: Always 0.
  */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
+
 int _isupper(int c)
 {
 	if(isupper(c))
