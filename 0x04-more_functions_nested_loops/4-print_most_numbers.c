590181848d70c85eb4643c93947f1c30bdef7437
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-23 11:12:15 -0800

    CREATE: 4-main.c 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - entry point
+ * Prints the numbers with _putchar
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number == '2' || number == '4')
+			continue;
+		else
+			_putchar(number);
+	}
+	_putchar('\n');
+}
