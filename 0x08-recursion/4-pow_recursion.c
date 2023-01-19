bc23799aa7e12012d99740a46e275b2bbd600187
Ailton Baúque <ailton.bauque@gmail.com>
2023-01-19 02:30:09 -0800

    UPDATE: task 4 pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - a power of to numbers, x power y
+ * @y: int
+ * @x: int
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < -1)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return ( x * _pow_recursion(x,y-1));
+}
