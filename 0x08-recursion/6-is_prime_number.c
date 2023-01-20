ce41d6489e03726f151a78b61cbd10f81e0d364d
Ailton Baúque <ailton.bauque@gmail.com>
2023-01-19 22:40:30 -0800

    UPDATE: betty coding style

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,11 +10,11 @@
  */
 int prime2(int a, int b)
 {
-  if (a == b)
-    return (1);
-  else if (a % b == 0)
-    return (0);
-  return (prime2(a, b + 1));
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
 }
 /**
  * is_prime_number - checks if a number is prime
@@ -25,7 +25,7 @@ int prime2(int a, int b)
  */
 int is_prime_number(int n)
 {
-  if (n <= 1)
-    return (0);
-  return (prime2(n, 2));
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
 }
